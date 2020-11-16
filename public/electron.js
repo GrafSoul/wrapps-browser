a92fddb7007146abeb52ffdc5ecb33fbb9b98a2b
GrafSoul <grafsoul@gmail.com>
2020-11-16 14:39:43 +0500

    feat: Added new functionality.

--- public/electron.js
+++ public/electron.js
@@ -1,22 +1,27 @@
-
-const { app, BrowserWindow, ipcMain} = require("electron");
-const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
-const path = require("path");
-const isDev = require("electron-is-dev");
+const { app, BrowserWindow, ipcMain } = require('electron');
+const {
+    default: installExtension,
+    REACT_DEVELOPER_TOOLS,
+    REDUX_DEVTOOLS,
+} = require('electron-devtools-installer');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
 const getIcon = () => {
-    if (process.platform === 'win32') return `${path.join(__dirname, "/icons/icon.ico")}`;
-    if (process.platform === 'darwin') return `${path.join(__dirname, "/icons/icon.icns")}`;
-    return `${path.join(__dirname, "/icons/16x16.png")}`;
+    if (process.platform === 'win32')
+        return `${path.join(__dirname, '/icons/icon.ico')}`;
+    if (process.platform === 'darwin')
+        return `${path.join(__dirname, '/icons/icon.icns')}`;
+    return `${path.join(__dirname, '/icons/16x16.png')}`;
 };
 
 let mainWindow;
 
-const createWindow = async () => {   
+const createWindow = async () => {
     mainWindow = new BrowserWindow({
-        title: 'Mnogo.uz',
+        title: 'Mnogo.uz 1.1.0',
         width: 1280,
         height: 720,
         minWidth: 340,
@@ -25,51 +30,63 @@ const createWindow = async () => {
         show: false,
         focusable: true,
         fullscreenable: false,
-        alwaysOnTop: true,
         icon: getIcon(),
         backgroundColor: '#282c34',
         webPreferences: {
             webviewTag: true,
             nodeIntegration: true,
-            webSecurity: false
-        }
+            enableRemoteModule: true,
+            webSecurity: false,
+        },
     });
 
     mainWindow.loadURL(
         isDev
-            ? "http://localhost:3000"
-            : `file://${path.join(__dirname, "../build/index.html")}`
+            ? 'http://localhost:3000'
+            : `file://${path.join(__dirname, '../build/index.html')}`,
     );
+
+    mainWindow.setAlwaysOnTop(true);
 
     mainWindow.once('ready-to-show', () => {
         mainWindow.show();
-        mainWindow.webContents.send('urlOpen', 'Mnogo.uz', 'http://video.mnogo.uz/'); 
+        mainWindow.webContents.send(
+            'urlOpen',
+            'Mnogo.uz',
+            'http://video.mnogo.uz/',
+        );
         if (isDev) {
             installExtension(REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS);
             mainWindow.webContents.openDevTools();
         }
     });
 
-    mainWindow.on("closed", () => (mainWindow = null));
-
-    
+    mainWindow.on('closed', () => (mainWindow = null));
 };
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
     if (mainWindow === null) {
         createWindow();
     }
 });
 
-ipcMain.on('channalOne', (event, args) => { 
-  console.log(args);
-  event.sender.send('channalOne','Message resieved on the main process')
-})
+ipcMain.on('on-top-browser', (event, id, args) => {
+    mainWindow.setAlwaysOnTop(args);
+});
+
+ipcMain.on('on-devtools', (event) => {
+    mainWindow.openDevTools();
+});
+
+ipcMain.on('channalOne', (event, args) => {
+    console.log(args);
+    event.sender.send('channalOne', 'Message resieved on the main process');
+});
--- src/app/App.js
+++ src/app/App.js
@@ -1,44 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import classes from  './App.module.scss';
+import classes from './App.module.scss';
 
 const { remote } = window.require('electron');
 const mainWindow = remote.getCurrentWindow();
 const electron = window.require('electron');
-const ipcRenderer  = electron.ipcRenderer;
+const ipcRenderer = electron.ipcRenderer;
 
-const App = () => {   
-	const [status, setStatus] = useState(false)
-	const [title, setTitle] = useState('Wrapps Browser')
-    const [url, setUrl] = useState('https://google.com')
-    const [isLoading, setLoading] = useState(false)
-    const [topbar, setTopbar] = useState(false)
+const App = () => {
+    const [status, setStatus] = useState(false);
+    // eslint-disable-next-line
+    const [id, setId] = useState('');
+    const [title, setTitle] = useState('Wrapps Browser');
+    const [url, setUrl] = useState('https://google.com');
+    const [isLoading, setLoading] = useState(false);
+    const [topbar, setTopbar] = useState(false);
+    const [isTop, setIsTop] = useState(false);
     const [webview, setWebview] = useState(document.querySelector('webview'));
 
     useEffect(() => {
-        setWebview(document.querySelector('webview'));		
+        setWebview(document.querySelector('webview'));
         ipcRenderer.on('urlOpen', (event, title, url) => {
             if (url.length !== 0) {
                 setUrl(url);
                 setTitle(title);
-            }    
-        })
+            }
+        });
     }, []);
 
     const handleGoBack = () => {
         webview.goBack();
     };
 
-    const handleGoForward = () => { 
+    const handleGoForward = () => {
         webview.goForward();
-    }
+    };
 
     const handleReload = () => {
         if (isLoading) {
-                webview.stop();
-                setLoading(false)
+            webview.stop();
+            setLoading(false);
         } else {
-                webview.reload();
-                setLoading(true)
+            webview.reload();
+            setLoading(true);
         }
     };
 
@@ -47,26 +50,39 @@ const App = () => {
     };
 
     const handleMaximizeWindow = () => {
-
         if (status) {
             mainWindow.unmaximize();
             setStatus(!status);
-        } else { 
+        } else {
             mainWindow.maximize();
             setStatus(!status);
-        }		
+        }
     };
 
     const handleCloseWindow = () => {
         mainWindow.close();
     };
 
-    const handleTopbarUp = () => {      
-       setTopbar(true)
+    const handleTopbarUp = () => {
+        setTopbar(true);
     };
 
-    const handleTopbarDown = () => {    
-       setTopbar(false)
+    const handleTopbarDown = () => {
+        setTopbar(false);
+    };
+
+    const handlerTopWindow = (id) => {
+        ipcRenderer.send('on-top-browser', id, true);
+        setIsTop(!isTop);
+    };
+
+    const handlerDownWindow = (id) => {
+        ipcRenderer.send('on-top-browser', id, false);
+        setIsTop(!isTop);
+    };
+
+    const handlerDevToolsWindow = () => {
+        ipcRenderer.send('on-devtools');
     };
 
     const top = [classes.webviewContainer, topbar ? classes.up : ''];
@@ -74,28 +90,87 @@ const App = () => {
     return (
         <div className={classes.layout}>
             <div className={classes.topbarWrap}>
-                <div className={classes.topbar} onMouseEnter={handleTopbarUp} onMouseLeave={handleTopbarDown}>
-                    
+                <div
+                    className={classes.topbar}
+                    onMouseEnter={handleTopbarUp}
+                    onMouseLeave={handleTopbarDown}
+                >
                     <div>
-                        <button className={classes.btnWindow} onClick={handleGoBack}><i className="fal fa-chevron-circle-left" /></button> 
-                        <button className={classes.btnWindow} onClick={handleGoForward}><i className="fal fa-chevron-circle-right"/></button>
-                        <button className={classes.btnWindow} onClick={handleReload}><i className="fal fa-sync"/></button>
+                        <button
+                            className={classes.btnWindow}
+                            onClick={handleGoBack}
+                        >
+                            <i className="fal fa-chevron-circle-left" />
+                        </button>
+                        <button
+                            className={classes.btnWindow}
+                            onClick={handleGoForward}
+                        >
+                            <i className="fal fa-chevron-circle-right" />
+                        </button>
+                        <button
+                            className={classes.btnWindow}
+                            onClick={handleReload}
+                        >
+                            <i className="fal fa-sync" />
+                        </button>
                     </div>
-                    <div className={classes.title}>{title} - <span>{url}</span></div>
+                    <div className={classes.title}>
+                        {title} - <span>{url}</span>
+                    </div>
                     <div>
-                        <button className={classes.btnWindow} onClick={handleMinimizeWindow}><i className="fal fa-window-minimize" /></button> 
-                        <button className={classes.btnWindow} onClick={handleMaximizeWindow}><i className="fal fa-window-maximize" /></button>
-                        <button className={classes.btnWindow + " " + classes.closeWindow} onClick={handleCloseWindow}><i className="fal fa-window-close" /></button>
+                        <button
+                            className={classes.btnWindow}
+                            onClick={handlerDevToolsWindow}
+                        >
+                            <i className="fal fa-tools" />
+                        </button>
+                        {isTop ? (
+                            <button
+                                className={classes.btnWindow}
+                                onClick={() => handlerDownWindow(id)}
+                                title="Unpin on top of all"
+                            >
+                                <i className="fal fa-arrow-from-top"></i>
+                            </button>
+                        ) : (
+                            <button
+                                className={classes.btnWindow}
+                                onClick={() => handlerTopWindow(id)}
+                                title="Pin on top of all"
+                            >
+                                <i className="fal fa-arrow-to-top"></i>
+                            </button>
+                        )}
+                        <button
+                            className={classes.btnWindow}
+                            onClick={handleMinimizeWindow}
+                        >
+                            <i className="fal fa-window-minimize" />
+                        </button>
+                        <button
+                            className={classes.btnWindow}
+                            onClick={handleMaximizeWindow}
+                        >
+                            <i className="fal fa-window-maximize" />
+                        </button>
+                        <button
+                            className={
+                                classes.btnWindow + ' ' + classes.closeWindow
+                            }
+                            onClick={handleCloseWindow}
+                        >
+                            <i className="fal fa-window-close" />
+                        </button>
                     </div>
-                </div>					
+                </div>
             </div>
-        
+
             <main className={classes.content}>
-                <webview src={url}
-                className={top.join(' ')}></webview>    
+                <webview src={url} className={top.join(' ')}></webview>
             </main>
         </div>
-    );  
-}
+    );
+};
 
 export default App;
